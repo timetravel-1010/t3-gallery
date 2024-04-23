e89b22dc0d764350f020237bb37152d19016a632
timetravel-1010 <timetravel_1010@protonmail.com>
2024-04-22 21:14:53 -0500

    Move to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ const Images = async () => {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-10 justify-center">
-      {images.map((image) => (
+    <div className="flex flex-wrap gap-4 justify-center p-4">
+      {[...images, ...images, ...images].map((image) => (
         <div key={image.id} className="flex flex-col h-48 w-48 ">
           <Link href={`/img/${image.id}`}>
             <Image
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -5,15 +5,15 @@ export default async function FullPageImageView(props: { id: number }) {
 
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex-shrink">
+      <div className="flex-shrink flex justify-center items-center">
         <img
           src={image.url}
-          className="w-96 object-contain"
+          className="flex-shrink object-contain"
           alt={image.name}
         />
       </div>
 
-      <div className="w-48 flex flex-col flex-shrink-0">
+      <div className="w-48 flex flex-col flex-shrink-0 border-l">
         <h3 className="text-xl font-bold">{image.name}</h3>
       </div>
     </div>
